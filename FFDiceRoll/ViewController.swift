17a99064346c759f016454e663ff727337103fc9
Nicholas Nuing <nicholas.nuing@gmail.com>
2017-09-20 17:52:57 +0800

    Generated random number using GamePlayKit.

--- FFDiceRoll/ViewController.swift
+++ FFDiceRoll/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import GameplayKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    var diceResult: GKRandomDistribution?
+    var pickerRow: Int? = 0
+    var ds: Int? = 0
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row] as String
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // print(row)
+        pickerRow = row
+    }
+    
+    @IBOutlet weak var diceUIPicker: UIPickerView!
+    @IBOutlet weak var rollDiceResult: UILabel!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.diceUIPicker.delegate = self
+        self.diceUIPicker.dataSource = self
+        
+        pickerData = [
+            "6 Sided",
+            "10 Sided",
+            "20 Sided"
+        ]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +56,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rollDiceAction(_ sender: Any) {
+        let pickerResult = UInt32(pickerRow!)
+        
+        switch pickerResult {
+            case 0:
+                diceResult = GKRandomDistribution.d6()
+            case 1:
+                diceResult = GKRandomDistribution.init(forDieWithSideCount: 10)
+            case 2:
+                diceResult = GKRandomDistribution.d20()
+            default: ds = 6
+        }
+        
+        let dc = Int((diceResult?.nextInt())!)
+        print(dc)
+        rollDiceResult.text = String(describing: dc)
+    }
 
 }
 
