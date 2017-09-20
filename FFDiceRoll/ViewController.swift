ab8e54cfeb0b1ed73b8482d6926e85e02da9ae66
Nicholas Nuing <nicholas.nuing@gmail.com>
2017-09-20 12:52:23 +0800

    Initial Commit

---
+++ FFDiceRoll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FFDiceRoll
+//
+//  Created by Nicholas Nuing on 20/09/2017.
+//  Copyright © 2017 otakrosak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
